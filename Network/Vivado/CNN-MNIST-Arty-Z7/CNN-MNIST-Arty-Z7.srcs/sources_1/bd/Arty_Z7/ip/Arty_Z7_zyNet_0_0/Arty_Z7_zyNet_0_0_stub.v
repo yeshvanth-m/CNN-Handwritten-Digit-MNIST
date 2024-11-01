c278bcfba52ac5a72b2d44f9737fa74da4a1b375
YM2 <Yeshvanth.Muniraj@analog.com>
2024-11-01 15:17:11 +0530

    Added network Vivado project

---
+++ Network/Vivado/CNN-MNIST-Arty-Z7/CNN-MNIST-Arty-Z7.srcs/sources_1/bd/Arty_Z7/ip/Arty_Z7_zyNet_0_0/Arty_Z7_zyNet_0_0_stub.v
@@ -0,0 +1,48 @@
+// Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2018.3 (win64) Build 2405991 Thu Dec  6 23:38:27 MST 2018
+// Date        : Wed Oct 30 00:42:33 2024
+// Host        : HYB-HIrJniKjkDx running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub
+//               c:/Workspace/Masters/neuralNetwork/Tut-8/myProject1/myProject1.srcs/sources_1/bd/Arty_Z7/ip/Arty_Z7_zyNet_0_0/Arty_Z7_zyNet_0_0_stub.v
+// Design      : Arty_Z7_zyNet_0_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z020clg400-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* X_CORE_INFO = "zyNet,Vivado 2018.3" *)
+module Arty_Z7_zyNet_0_0(s_axi_aclk, s_axi_aresetn, axis_in_data, 
+  axis_in_data_valid, axis_in_data_ready, s_axi_awaddr, s_axi_awprot, s_axi_awvalid, 
+  s_axi_awready, s_axi_wdata, s_axi_wstrb, s_axi_wvalid, s_axi_wready, s_axi_bresp, 
+  s_axi_bvalid, s_axi_bready, s_axi_araddr, s_axi_arprot, s_axi_arvalid, s_axi_arready, 
+  s_axi_rdata, s_axi_rresp, s_axi_rvalid, s_axi_rready, intr)
+/* synthesis syn_black_box black_box_pad_pin="s_axi_aclk,s_axi_aresetn,axis_in_data[7:0],axis_in_data_valid,axis_in_data_ready,s_axi_awaddr[4:0],s_axi_awprot[2:0],s_axi_awvalid,s_axi_awready,s_axi_wdata[31:0],s_axi_wstrb[3:0],s_axi_wvalid,s_axi_wready,s_axi_bresp[1:0],s_axi_bvalid,s_axi_bready,s_axi_araddr[4:0],s_axi_arprot[2:0],s_axi_arvalid,s_axi_arready,s_axi_rdata[31:0],s_axi_rresp[1:0],s_axi_rvalid,s_axi_rready,intr" */;
+  input s_axi_aclk;
+  input s_axi_aresetn;
+  input [7:0]axis_in_data;
+  input axis_in_data_valid;
+  output axis_in_data_ready;
+  input [4:0]s_axi_awaddr;
+  input [2:0]s_axi_awprot;
+  input s_axi_awvalid;
+  output s_axi_awready;
+  input [31:0]s_axi_wdata;
+  input [3:0]s_axi_wstrb;
+  input s_axi_wvalid;
+  output s_axi_wready;
+  output [1:0]s_axi_bresp;
+  output s_axi_bvalid;
+  input s_axi_bready;
+  input [4:0]s_axi_araddr;
+  input [2:0]s_axi_arprot;
+  input s_axi_arvalid;
+  output s_axi_arready;
+  output [31:0]s_axi_rdata;
+  output [1:0]s_axi_rresp;
+  output s_axi_rvalid;
+  input s_axi_rready;
+  output intr;
+endmodule
